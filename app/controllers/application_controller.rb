c268220cf7a7bf3edd2f0c6740e15a7f803d402b
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2023-06-09 15:42:17 +0530

    application level files, Gemfile and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal
+
+# app > controllers > application_controller
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :events
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "events#index"
 end
